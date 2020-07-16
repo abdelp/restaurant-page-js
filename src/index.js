95ac14e492ae2003b094dc345c431f14541a87ce
Abdel <juniorperezpy@gmail.com>
2020-07-16 11:52:44 -0400

    Add tabsy-css package

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import * as Home from './modules/home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const contentId = Config.getContentId();
-Display.cleanContent(contentId);
 Display.setBackgroundImage(contentId, Config.getBackgroundImg());
 Display.insertNav(Config.getLinks());
 Home.loadHome();
--- src/modules/menu.js
+++ src/modules/menu.js
@@ -2,11 +2,7 @@ import * as Display from './display';
 import * as Config from './configurations';
 
 const loadMenu = () => {
-  const contentId = Config.getContentId();
-  Display.cleanContent(contentId);
   Display.insertTitle('Menu');
-  Display.setBackgroundImage(contentId, Config.getBackgroundImg());
-  Display.insertNav(Config.getLinks());
 };
 
 export { loadMenu };
